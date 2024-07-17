5b79313a253b76800f200d6c53d89dfa8cdb64b2
iKNZR <erickson.1207@gmail.com>
2024-07-17 18:39:20 -0500

    Login

--- screens/auth/sign-in/signin.screen.tsx
+++ screens/auth/sign-in/signin.screen.tsx
@@ -1,10 +1,294 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  ScrollView,
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Button,
+  Platform,
+} from "react-native";
+import {
+  AntDesign,
+  Entypo,
+  FontAwesome,
+  Fontisto,
+  Ionicons,
+  SimpleLineIcons,
+} from "@expo/vector-icons";
+import { useFonts } from "expo-font";
+import { LinearGradient } from "expo-linear-gradient";
+import { router } from "expo-router";
+import { transform } from "@babel/core";
+import React, { useState } from "react";
+import { welcomeIntroSwipperData } from "@/constants/constants";
+import AppIntroSlider from "react-native-app-intro-slider";
+import {
+  responsiveHeight,
+  responsiveWidth,
+} from "react-native-responsive-dimensions";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { CheckBox } from "react-native-elements";
+
+import { Toast } from "react-native-toast-notifications";
 
 export default function SignInScreen() {
+  let [fontsLoaded, fontError] = useFonts({
+    "Cherione Bold": require("../../../assets/fonts/Cherione Bold.ttf"),
+    "Cherione Normal": require("../../../assets/fonts/Cherione Normal.ttf"),
+    "Cherione Light": require("../../../assets/fonts/Cherione Light.ttf"),
+    "Cherione Regular": require("../../../assets/fonts/Cherione.otf"),
+    "Geomanist Regular": require("../../../assets/fonts/Geomanist-Regular.otf"),
+    "Geomanist Bold": require("../../../assets/fonts/Geomanist-Bold.otf"),
+    "Geomanist Light": require("../../../assets/fonts/Geomanist-Light.otf"),
+    "Geomanist Medium": require("../../../assets/fonts/Geomanist-Medium.otf"),
+    "Geomanist Thin": require("../../../assets/fonts/Geomanist-Thin.otf"),
+    "Geomanist ExtraLight": require("../../../assets/fonts/Geomanist-ExtraLight.otf"),
+    "Geomanist Ultra": require("../../../assets/fonts/Geomanist-Ultra.otf"),
+    ...FontAwesome.font,
+  });
+  const [isPasswordVisible, setPasswordVisible] = useState(false);
+  const [buttonSpinner, setButtonSpinner] = useState(false);
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    password: "",
+  });
+  const [required, setRequired] = useState("");
+  const [error, setError] = useState({
+    password: "",
+  });
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  const handlePasswordValidation = (value: string) => {
+    const password = value;
+    const passwordSpecialCharacter = /(?=.*[!@#$&*])/;
+    const passwordOneNumber = /(?=.*[0-9])/;
+    const passwordSixValue = /(?=.{6,})/;
+
+    if (!passwordSpecialCharacter.test(password)) {
+      setError({
+        ...error,
+        password: "Write at least one special character",
+      });
+      setUserInfo({ ...userInfo, password: "" });
+    } else if (!passwordOneNumber.test(password)) {
+      setError({
+        ...error,
+        password: "Write at least one number",
+      });
+      setUserInfo({ ...userInfo, password: "" });
+    } else if (!passwordSixValue.test(password)) {
+      setError({
+        ...error,
+        password: "Write at least 6 characters",
+      });
+      setUserInfo({ ...userInfo, password: "" });
+    } else {
+      setError({
+        ...error,
+        password: "",
+      });
+      setUserInfo({ ...userInfo, password: value });
+    }
+  };
+
   return (
-    <View>
-      <Text>SignInScreen</Text>
-    </View>
+    <LinearGradient
+      colors={["#F9F6F7", "#F9F6F7"]}
+      style={{ flex: 1, paddingTop: 30 }}
+    >
+      <Text style={styles.topText}>LOGEO</Text>
+      <View
+        style={{ backgroundColor: "#F9F6F7", flex: 1, alignContent: "center", justifyContent: "center" }}
+      >
+        <Image
+          source={require("@/assets/images/CHARRO_NEGRO-03.png")}
+          style={[
+            styles.signInImage,
+            { backgroundColor: "#F9F6F7", opacity: 0.67 },
+          ]}
+        />
+        <View style={styles.inputContainer}>
+          <View>
+            <TextInput
+              style={[styles.input, {}]}
+              keyboardType="default"
+              value={userInfo.name}
+              placeholder="Nombre de Usuario"
+              onChangeText={(value) =>
+                setUserInfo({ ...userInfo, name: value })
+              }
+            />
+            <AntDesign
+              style={{ position: "absolute", left: 26, top: 12 }}
+              name="user"
+              size={20}
+              color="#A1A1A1"
+            />
+          </View>  
+          <View>
+            <TextInput
+              style={[styles.input, { paddingTop: 0, paddingLeft: 35 }]}
+              keyboardType="default"
+              value={userInfo.password}
+              secureTextEntry={!isPasswordVisible}
+              placeholder="••••••••••"
+              onChangeText={(value) =>
+                setUserInfo({ ...userInfo, password: value })
+              }
+            />
+            <TouchableOpacity
+              style={styles.visibleIcon}
+              onPress={() => setPasswordVisible(!isPasswordVisible)}
+            >
+              {isPasswordVisible ? (
+                <Ionicons name="eye-off-outline" size={23} color={"#A1A1A1"} />
+              ) : (
+                <Ionicons name="eye-outline" size={23} color={"#A1A1A1"} />
+              )}
+            </TouchableOpacity>
+            <SimpleLineIcons
+              style={{ position: "absolute", left: 26, top: 12 }}
+              name="lock"
+              size={20}
+              color="#A1A1A1"
+            />
+          </View>
+         
+        </View>
+        <TouchableOpacity style={[styles.button, {paddingLeft: -35, marginHorizontal: 32, marginTop: 15}]}>
+          <Text
+            style={[
+              {
+                color: "white",
+                marginTop: 11,
+                fontSize: 16,
+                fontFamily: "Cherione Regular",
+                textAlign: "center",
+              },
+            ]}
+            onPress={() => router.push("/(routes)/verify-account")}
+          >
+            INGRESAR
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
   )
 }
+
+
+export const styles = StyleSheet.create({
+  checkboxContainer: {
+    backgroundColor: "#f9f9f9",
+    borderWidth: 0,
+    marginTop: -5,
+    marginLeft: 25,
+    width: "80%",
+    textAlign: "left",
+    padding: 0,
+  },
+  checkboxText: {
+    color: "#A1A1A1",
+    fontSize: 12,
+    fontFamily: "Geomanist Regular",
+  },
+  imageContainer: {
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: "100%",
+  },
+  inputContainer: {
+    marginHorizontal: 16,
+    marginTop: -40,
+    rowGap: 20,
+  },
+  input: {
+    height: 45,
+    marginHorizontal: 16,
+    borderRadius: 15,
+    borderColor: "#A1A1A1",
+    borderWidth: 0.8,
+    paddingLeft: 35,
+    fontSize: 16,
+    fontFamily: "Geomanist Regular",
+    backgroundColor: "white",
+    color: "#A1A1A1",
+  },
+  button: {
+    height: 45,
+    marginHorizontal: 16,
+    borderRadius: 15,
+    borderColor: "#A1A1A1",
+    borderWidth: 0.8,
+    paddingLeft: 35,
+    backgroundColor: "#A1A1A1",
+  },
+  signInImage: {
+    width: "60%",
+    height: 250,
+    alignSelf: "center",
+    marginTop: -180,
+  },
+  topText: {
+    display: "flex",
+    paddingTop: 15,
+    paddingBottom: 8,
+    paddingLeft: 15,
+    fontFamily: "Geomanist Regular",
+    borderBottomColor: "#949494",
+    borderBottomWidth: 1,
+    color: "#949494",
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "left",
+    marginTop: 11,
+    fontSize: 16,
+    fontFamily: "Geomanist Regular",
+  },
+  welcomeButtonStyle: {
+    flex: 1,
+    backgroundColor: "transparent",
+    width: "25%",
+    alignSelf: "flex-end",
+    borderRadius: 15,
+    bottom: 28,
+  },
+  dotStyle: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignContent: "flex-end",
+    alignItems: "flex-end",
+    backgroundColor: "transparent",
+    left: responsiveWidth(-35),
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    borderColor: "white",
+    borderWidth: 1,
+    bottom: -5,
+  },
+  activeDotStyle: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignContent: "flex-end",
+    alignItems: "flex-end",
+    backgroundColor: "white",
+    left: responsiveWidth(-35),
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    bottom: -5,
+  },
+  visibleIcon: {
+    position: "absolute",
+    right: 30,
+    top: 11,
+  },
+});
--- screens/auth/verify/verify.account.tsx
+++ screens/auth/verify/verify.account.tsx
@@ -34,6 +34,23 @@ import { CheckBox } from "react-native-elements";
 import { Toast } from "react-native-toast-notifications";
 
 export default function VerifyAccountScreen() {
+  let [fontsLoaded, fontError] = useFonts({
+    "Cherione Bold": require("../../../assets/fonts/Cherione Bold.ttf"),
+    "Cherione Normal": require("../../../assets/fonts/Cherione Normal.ttf"),
+    "Cherione Light": require("../../../assets/fonts/Cherione Light.ttf"),
+    "Cherione Regular": require("../../../assets/fonts/Cherione.otf"),
+    "Geomanist Regular": require("../../../assets/fonts/Geomanist-Regular.otf"),
+    "Geomanist Bold": require("../../../assets/fonts/Geomanist-Bold.otf"),
+    "Geomanist Light": require("../../../assets/fonts/Geomanist-Light.otf"),
+    "Geomanist Medium": require("../../../assets/fonts/Geomanist-Medium.otf"),
+    "Geomanist Thin": require("../../../assets/fonts/Geomanist-Thin.otf"),
+    "Geomanist ExtraLight": require("../../../assets/fonts/Geomanist-ExtraLight.otf"),
+    "Geomanist Ultra": require("../../../assets/fonts/Geomanist-Ultra.otf"),
+    ...FontAwesome.font,
+  });
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   const [code, setCode] = useState(new Array(4).fill(""));
 
   const inputs = useRef<any>([...Array(4)].map(() => React.createRef()));
@@ -81,9 +98,9 @@ export default function VerifyAccountScreen() {
     >
       <Text style={styles.topText}>REGISTRO</Text>
       <View style={styles.container}>
-        <Text style={styles.headerText}>Verification Code</Text>
+        <Text style={styles.headerText}>CÓDIGO DE VERIFICACIÓN</Text>
         <Text style={styles.subText}>
-          We have sent the verification code to your email address
+          Te hemos mandando un código de verificación a tu correo electrónico
         </Text>
         <View style={styles.inputContainer}>
           {code.map((_, index) => (
@@ -99,10 +116,23 @@ export default function VerifyAccountScreen() {
             />
           ))}
         </View>
-        <View style={{ marginTop: 10 }}>
-          <Button title="Submit" onPress={handleSumbit} />
-        </View>
-        <View style={styles.loginLink}>
+        <TouchableOpacity style={[styles.button, {paddingLeft: -35, marginHorizontal: 32, marginTop: 15}]}>
+          <Text
+            style={[
+              {
+                color: "white",
+                marginTop: 11,
+                fontSize: 16,
+                fontFamily: "Cherione Regular",
+                textAlign: "center",
+              },
+            ]}
+            onPress={() => router.push("/(routes)/sign-in")}
+          >
+            VERIFICAR
+          </Text>
+        </TouchableOpacity>
+        {/* <View style={styles.loginLink}>
           <Text style={[styles.backText, { fontFamily: "Nunito_700Bold" }]}>
             Back To?
           </Text>
@@ -111,13 +141,23 @@ export default function VerifyAccountScreen() {
               Sign In
             </Text>
           </TouchableOpacity>
-        </View>
+        </View> */}
       </View>
     </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
+  button: {
+    height: 45,
+    width: "50%",
+    marginHorizontal: 16,
+    borderRadius: 15,
+    borderColor: "#A1A1A1",
+    borderWidth: 0.8,
+    paddingLeft: 35,
+    backgroundColor: "#A1A1A1",
+  },
   topText: {
     display: "flex",
     paddingTop: 15,
@@ -134,17 +174,20 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     padding: 20,
     backgroundColor: "#fff",
+    color: "#949494",
   },
   headerText: {
     fontSize: 22,
-    fontWeight: "bold",
+    fontFamily: "Cherione Normal",
+    color: "#949494",
     marginBottom: 10,
   },
   subText: {
     fontSize: 16,
-    color: "#666",
+    color: "#949494",
     marginBottom: 20,
     textAlign: "center",
+    fontFamily: "Geomanist Regular",
   },
   inputContainer: {
     flexDirection: "row",
