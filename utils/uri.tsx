161165b9e64b49c7ca0fb34ee403cae366af4b67
0800WEB <144950728+lucasidiotlab@users.noreply.github.com>
2024-09-18 17:45:53 -0300

    api maps

--- utils/uri.tsx
+++ utils/uri.tsx
@@ -2,4 +2,4 @@
 export const SERVER_URI =
   process.env.SERVER_URI || "https://delyapp-back2.onrender.com";
 
-export const GOOGLE_MAPS_APIKEY = "AIzaSyCxp4-dyjYWfEkugeZFkE8MOEHtvyQG17s"
+export const GOOGLE_MAPS_APIKEY = "AIzaSyAQBdWh0ccFVFusZHEJVGOYDwdBRqFoIw8"
